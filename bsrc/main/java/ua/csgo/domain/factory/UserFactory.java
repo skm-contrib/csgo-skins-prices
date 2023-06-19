a9e26af3d03ce3a889cecdce763218f7ece69b1f
uzbekowms <lukasowms@gmail.com>
2023-06-19 18:11:46 +0300

    Updating user

--- bsrc/main/java/ua/csgo/domain/factory/UserFactory.java
+++ bsrc/main/java/ua/csgo/domain/factory/UserFactory.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import ua.csgo.domain.model.User;
+import ua.csgo.domain.repository.UserRepository;
 import ua.csgo.web.dto.AuthDTO;
 import ua.csgo.web.dto.UserDTORequest;
 import ua.csgo.web.dto.UserDTOResponse;
@@ -13,14 +14,16 @@ import ua.csgo.web.dto.UserDTOResponse;
 public class UserFactory {
 
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     public User fromDto(UserDTORequest request) {
+        User user = userRepository.findByEmail(request.getEmail()).orElseThrow();
         return User.builder()
                 .id(request.getId())
                 .nickname(request.getNickname())
                 .email(request.getEmail())
                 .role(request.getRole())
-                .password(request.getPassword())
+                .password(request.getPassword() == null ? user.getPassword() : request.getPassword())
                 .build();
     }
 
